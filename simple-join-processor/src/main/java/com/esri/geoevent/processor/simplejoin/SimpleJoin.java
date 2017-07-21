08e8279a4cc4e044d1b16d8b17df393e4d822ee1
Morakot Pilouk <mpilouk@esri.com>
2017-07-21 11:00:18 -0700

    Added copyright text
    Added copyright text

--- simple-join-processor/src/main/java/com/esri/geoevent/processor/simplejoin/SimpleJoin.java
+++ simple-join-processor/src/main/java/com/esri/geoevent/processor/simplejoin/SimpleJoin.java
@@ -1,8 +1,31 @@
+/*
+  Copyright 1995-2017 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  For additional information, contact:
+  Environmental Systems Research Institute, Inc.
+  Attn: Contracts Dept
+  380 New York Street
+  Redlands, California, USA 92373
+
+  email: contracts@esri.com
+ */
+
 package com.esri.geoevent.processor.simplejoin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +40,8 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 import com.esri.ges.core.Uri;
 import com.esri.ges.core.component.ComponentException;
-import com.esri.ges.core.geoevent.DefaultFieldDefinition;
-import com.esri.ges.core.geoevent.FieldCardinality;
 import com.esri.ges.core.geoevent.FieldDefinition;
 import com.esri.ges.core.geoevent.FieldExpression;
-import com.esri.ges.core.geoevent.FieldType;
 import com.esri.ges.core.geoevent.GeoEvent;
 import com.esri.ges.core.geoevent.GeoEventDefinition;
 import com.esri.ges.core.geoevent.GeoEventPropertyName;
